e330a1648b8635500471636136b9dde4a38c942f
otal <otal@meadallia.com>
2019-08-25 18:37:36 +0300

    new version

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ExpenseListItem from './ExpenseListItem';
-import selectExpenses from '../selectors/expenses';
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseListItem from "./ExpenseListItem";
+import selectExpenses from "../selectors/expenses";
 
-export const ExpenseList = (props) => (
+export const ExpenseList = props => (
   <div>
-    { props.expenses.length === 0 ?
-      (<p>No Expenses</p>)
-      : (props.expenses.map((expense) => {
-        return <ExpenseListItem key={ expense.id } { ...expense } />;
+    <h1>Expense List</h1>
+    {props.expenses.length === 0 ? (
+      <p>No Expenses</p>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
       })
-      ) }
+    )}
+    <h3>Total Amount:{props.expenses.reduce((a, b) => parseFloat(a) + parseFloat(b.amount), 0).toFixed(2)}</h3>
   </div>
 );
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     expenses: selectExpenses(state.expenses, state.filters)
   };
