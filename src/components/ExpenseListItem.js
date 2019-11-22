78fbabfe192b53f685ad613e2f2ae9a01d50e81a
oran <orantal01@gmail.com>
2019-11-22 18:46:15 +0200

    bla

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
-import numeral from 'numeral';
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
-    </Link>
-    <p>
-      {numeral(amount).format('0,0.00')}
-      -
-      {moment(createdAt).format('DD MMMM, YYYY')}
-    </p>
-  </div>
-);
+const ExpenseListItem = ({ id, description, amount, createdAt }) => {
+  if (numeral.locales["user-locale"] === undefined) {
+    numeral.register("locale", "user-locale", {
+      delimiters: {
+        thousands: ",",
+        decimal: "."
+      },
+      abbreviations: {
+        thousand: "k",
+        million: "m",
+        billion: "b",
+        trillion: "t"
+      },
+      currency: {
+        symbol: "₪"
+      }
+    });
+    numeral.locale("user-locale");
+  }
+
+  const amountShekel = numeral(amount / 100).format("$0,0.00");
+  return (
+    <div>
+      <Link to={`/edit/${id}`}>
+        <h3>{description}</h3>
+      </Link>
+      <p>
+        {amountShekel}-{moment(createdAt).format("DD MMMM, YYYY")}
+      </p>
+    </div>
+  );
+};
 
 export default ExpenseListItem;
