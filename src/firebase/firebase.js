0a1e5ffd5c0c6587e606c6ca82b04ddabcb00705
oran <orantal01@gmail.com>
2019-11-25 00:03:02 +0200

    setup test and dev env database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,29 +1,30 @@
 import * as firebase from 'firebase';
-import expenses from '../tests/fixtures/expenses';
-
-// const config = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApzjadytp4fvhHUveoSEDBMbk-GzTkXXc",
-  authDomain: "expensify-a0662.firebaseapp.com",
-  databaseURL: "https://expensify-a0662.firebaseio.com",
-  projectId: "expensify-a0662",
-  storageBucket: "expensify-a0662.appspot.com",
-  messagingSenderId: "5000931356",
-  appId: "1:5000931356:web:a7433f3953998865"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyApzjadytp4fvhHUveoSEDBMbk-GzTkXXc",
+//   authDomain: "expensify-a0662.firebaseapp.com",
+//   databaseURL: "https://expensify-a0662.firebaseio.com",
+//   projectId: "expensify-a0662",
+//   storageBucket: "expensify-a0662.appspot.com",
+//   messagingSenderId: "5000931356",
+//   appId: "1:5000931356:web:a7433f3953998865"
+// };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
+
+export { firebase, database as default };
 
 // Setup "expenses" with three items (description, note, amount, createdAt)
 // database.ref('expenses').push({
